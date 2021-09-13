a964569d783eb2a183c2dc5624c4b7e7fb8cb8d9
decentralizedmvp <decentralizedmvp@gmail.com>
2021-09-13 09:06:56 -0400

    feat: add support for showing donate modal

--- client/src/pages/fund-raise/FundRaise.jsx
+++ client/src/pages/fund-raise/FundRaise.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Col, Row, Button } from 'react-bootstrap'
+import { Container, Col, Row, Button, Modal, Form } from 'react-bootstrap'
 import getImageForFundRaise from '../utils/get-image-for-fund-raise'
 
 import './FundRaise.css'
@@ -9,8 +9,11 @@ export default function FundRaise({
   id,
   description,
   current,
-  goal
+  goal,
+  donateModal
 }) {
+  const { onClick, show, onClose, onChange, onSubmit } = donateModal
+
   return (
     <Container fluid="lg">
       <h1 className="mb-3">{title}</h1>
@@ -22,12 +25,43 @@ export default function FundRaise({
           <div className="fund-raise-side-panel mt-2 mt-lg-0">
             <div className="fund-raise-goal">{current} raised of {goal}...</div>
             <div className="fund-raise-donate-button-container">
-              <Button variant="primary">Donate</Button>
+              <Button variant="primary" onClick={onClick}>Donate</Button>
             </div>
           </div>
         </Col>
       </Row>
       <p className="mt-3">{description}</p>
+      <DonateModal
+        onClose={onClose}
+        onChange={onChange}
+        onSubmit={onSubmit}
+        show={show}
+      />
     </Container>
+  )
+}
+
+function DonateModal({
+  show,
+  onClose,
+  onSubmit,
+  onChange
+}) {
+  return (
+    <Modal show={show} onHide={onClose}>
+      <Modal.Header closeButton onClick={onClose}>
+        <Modal.Title>Donate</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={onSubmit}>
+          <Form.Group controlId="amount">
+            <Form.Label>Amount</Form.Label>
+            <Form.Control type="text" name="amount" onChange={onChange}/>
+          </Form.Group>
+          <Button variant="secondary" onClick={onClose} className="me-1 mt-1">Close</Button>
+          <Button variant="primary" type="submit" className="mt-1">Submit</Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
   )
 }
--- client/src/pages/fund-raise/FundRaise.stories.jsx
+++ client/src/pages/fund-raise/FundRaise.stories.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FundRaise from './FundRaise';
 
 export default {
@@ -6,7 +6,23 @@ export default {
   component: FundRaise
 };
 
-const Template = (args) => <FundRaise {...args}/>;
+const Template = (args) => {
+  const [show, setShow] = useState(false)
+
+  const donateModal = {
+    show,
+    onChange: undefined,
+    onClick: () => setShow(true),
+    onClose: () => setShow(false),
+    onSubmit: event => event.preventDefault() 
+  }
+
+  args = { ...args, donateModal }
+
+  return (
+    <FundRaise {...args}/>
+  )
+};
 
 export const FundRaiseStory = Template.bind({});
 FundRaiseStory.args = {
@@ -14,5 +30,6 @@ FundRaiseStory.args = {
   id: 0,
   description: 'This is my first fund raiser for my dApp idea',
   current: 500,
-  goal: 3000
+  goal: 3000,
+  // donateModal: {} (defined above)
 };
